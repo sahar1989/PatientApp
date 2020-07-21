7aee037e7fc3806c676f0a3291624590f1b348d6
sahar1989 <sahar.hosseini@hotmail.com>
2020-07-21 18:36:12 +0200

    add war file

--- out/artifacts/PatientApp_war_exploded/WEB-INF/classes/sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `patientdb`.`patienttbl` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `age` INT NULL,
-  `birth_date` VARCHAR(12) NULL,
-  PRIMARY KEY (`Id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
